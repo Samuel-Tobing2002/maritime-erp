74b79e5ac33f98907ce277db15a300d533741ccb
Samuel-Tobing2002 <jaguarlangkah@gmail.com>
2026-04-15 14:37:03 +0700

    update code navbar

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -1,30 +1,35 @@
 <template>
   <div class="h-16 bg-[#F3F4FA] flex items-center justify-between px-6">
-    <h3 class="font-bold text-[35px] text-[#000348] pt-8">
+
+    <!-- TITLE -->
+    <h3 class="font-bold text-xl text-[#000348]">
       Dashboard
     </h3>
 
+    <!-- RIGHT -->
     <div class="flex items-center gap-4">
+
+      <!-- NOTIF -->
       <div class="relative cursor-pointer">
         <Bell class="w-5 h-5 text-black" />
-        <span class="absolute -top-1 -right-1 
-                     bg-red-500 text-white text-[10px] 
-                     w-4 h-4 flex items-center justify-center 
-                     rounded-full font-semibold">
+        <span class="absolute -top-1 -right-1 bg-red-500 text-white text-[10px] w-4 h-4 flex items-center justify-center rounded-full">
           2
         </span>
       </div>
 
+      <!-- PROFILE -->
       <div class="flex items-center gap-2 cursor-pointer">
-        <div class="w-9 h-9 rounded-lg bg-red-600 text-white 
-                    flex items-center justify-center text-sm font-bold">
+        <div class="w-9 h-9 rounded-lg bg-red-600 text-white flex items-center justify-center text-sm font-bold">
           KW
         </div>
+
         <span class="text-sm text-black font-medium">
           kelvinwilliam@gmail.com
-        </span
+        </span>
       </div>
+
     </div>
+
   </div>
 </template>
 
